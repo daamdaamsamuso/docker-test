501d1122e9a32bb0ad98acef3cddce45ea9c4bd8
root <root@ip-172-31-14-90.ap-northeast-2.compute.internal>
2021-02-15 11:44:26 +0000

    env setting

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $conn = mysqli_connect(
+    'docker-mysql-test.cn9besazrjuk.ap-northeast-2.rds.amazonaws.com',
+    'user',
+    'password',
+    'TEST',
+    '3306');
+    if(mysqli_connect_errno()){
+        echo "Failed to connect to MYSQL: ".mysqli_connect_error();
+    }
+    $sql = "SELECT VERSION()";
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($result);
+    print_r($row["VERSION()"]);
+   ?>
